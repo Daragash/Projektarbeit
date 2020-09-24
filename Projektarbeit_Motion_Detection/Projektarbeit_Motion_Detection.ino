63365c2fa00af29f32ed09ce004f76e673db96d6
Daragash <daragash@web.de>
2020-09-24 10:07:11 +0200

    Inits und Includes

--- Projektarbeit_Motion_Detection/Projektarbeit_Motion_Detection.ino
+++ Projektarbeit_Motion_Detection/Projektarbeit_Motion_Detection.ino
@@ -1,3 +1,20 @@
+#include "MPU6050_Constants.h"
+#include "Changeable_Settings.h"
+
+#include<Wire.h>
+
+const int MPU_addr = 0x68; // I2C address of the MPU-6050
+int16_t AcX, AcZ, GyY;
+
+float expFilterAlpha[3] = {largeAlpha, mediumAlpha, smallAlpha};
+
+
+float mediumExponentialFilterX;
+float mediumExponentialFilterZ;
+float mediumExponentialFilterGY;
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
